675fe0ce0f2724935cceeaef28317a7030632441
Gurwinder Singh <vargwin@gmail.com>
2024-05-04 14:08:33 +0530

    fix regression

--- src/request/json-editor.ts
+++ src/request/json-editor.ts
@@ -1,11 +1,12 @@
 import "../components/editor.js";
 
-import { LitElement, html } from "lit";
+import { MobxLitElement } from "@adobe/lit-mobx";
+import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { RequestBodyState } from "./state.js";
 
 @customElement("json-editor")
-export class JsonEditor extends LitElement {
+export class JsonEditor extends MobxLitElement {
   @property({ attribute: false })
   body!: RequestBodyState;
 
